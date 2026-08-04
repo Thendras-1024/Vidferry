f9b6add5390b67af1b5872f1396acee225f95dec
Thendras <Lyu.xidian@gmail.com>
2026-08-05 00:12:40 +0800

    test: 删除过时测试脚本

--- tests/test_postgres_compat.py
+++
@@ -1,7 +0,0 @@
-from app.db.postgres_compat import normalize_postgres_sql
-
-
-def test_postgres_sql_escapes_like_percent_and_converts_parameters():
-    sql = "SELECT * FROM file_records WHERE lower(filename) LIKE '%.mp4' AND id = ?"
-
-    assert normalize_postgres_sql(sql) == "SELECT * FROM file_records WHERE lower(filename) ILIKE '%%.mp4' AND id = %s"
